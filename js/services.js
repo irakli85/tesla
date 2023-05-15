8dbe62bd129ff18492934f03face3cad6c2504ad
iberidze38 <iberidze38@gmail.com>
2023-05-15 15:28:32 +0400

    services

---
+++ js/services.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.card').on('mouseenter', function(){
+        $(this).find('.card-title').css('color', '#675da9')
+    })
+
+    $('.card').on('mouseleave', function(){
+        $(this).find('.card-title').css('color', '')
+    })
+
+    $('.col-md-4').on('mouseenter', function(){
+        $(this).find('.card').css('box-shadow', 'rgba(0, 0, 0, 0.35) 0px 5px 15px')
+    })
+
+    $('.col-md-4').on('mouseleave', function(){
+        $(this).find('.card').css('box-shadow', '')
+    })
+})
